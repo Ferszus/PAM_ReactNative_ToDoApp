7f284c3cdda150b095b8a60b7de04232ddf859ef
Hubert Parylak <94863309+Ferszus@users.noreply.github.com>
2024-11-21 17:24:41 +0100

    Better styling

--- App.js
+++ App.js
@@ -141,10 +141,15 @@ export default function App() {
             {deadline ? `Deadline: ${deadline}` : 'Ustaw Deadline'}
           </Text>
         </TouchableOpacity>
-        <Button title="Dodaj" onPress={addTask} />
+        <TouchableOpacity style={styles.addButton} onPress={addTask}>
+          <Text style={styles.addButtonText}>Dodaj</Text>
+        </TouchableOpacity>
       </View>
+
       <Button title="Sortuj po dacie" onPress={sortTasksByDeadline} />
       <FlatList
+        style={styles.listContainer}
+        contentContainerStyle={{ paddingHorizontal: 10 }} // Dodano odstępy po bokach
         data={tasks}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
@@ -160,15 +165,13 @@ export default function App() {
                 : item.completed
                 ? styles.completedTaskContainer
                 : null,
-            ]}
-          >
+            ]}>
             <View>
               <Text
                 style={[
                   styles.taskText,
                   item.completed && styles.completedTaskText,
-                ]}
-              >
+                ]}>
                 {item.text}
               </Text>
               <Text style={styles.deadlineText}>Termin: {item.deadline}</Text>
@@ -193,9 +196,8 @@ export default function App() {
       {/* Modal do edycji zadania */}
       <Modal
         visible={editModalVisible}
-        animationType="slide"
-        transparent={true}
-      >
+        animationType="fade" // Dodano płynne przejście
+        transparent={true}>
         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
             <Text style={styles.modalTitle}>Edytuj zadanie</Text>
@@ -207,18 +209,17 @@ export default function App() {
             />
             <TouchableOpacity
               style={styles.dateButton}
-              onPress={showDatePicker}
-            >
+              onPress={showDatePicker}>
               <Text style={styles.dateButtonText}>
                 {deadline ? `Deadline: ${deadline}` : 'Ustaw Deadline'}
               </Text>
             </TouchableOpacity>
             <Button title="Zapisz zmiany" onPress={saveEditedTask} />
-            <Button
-              title="Anuluj"
-              color="red"
-              onPress={() => setEditModalVisible(false)}
-            />
+            <TouchableOpacity
+              style={styles.modalCancelButton}
+              onPress={() => setEditModalVisible(false)}>
+              <Text style={styles.modalCancelButtonText}>Anuluj</Text>
+            </TouchableOpacity>
           </View>
         </View>
       </Modal>
@@ -237,34 +238,38 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: '#f8f8f8',
+    backgroundColor: '#f4f7f9',
   },
   title: {
-    fontSize: 24,
+    fontSize: 26,
     fontWeight: 'bold',
     marginBottom: 20,
     textAlign: 'center',
+    color: '#333',
   },
   inputContainer: {
     marginBottom: 20,
   },
   input: {
-    borderColor: '#ccc',
+    borderColor: '#007bff',
     borderWidth: 1,
-    padding: 10,
-    borderRadius: 5,
+    padding: 12,
+    borderRadius: 8,
     backgroundColor: '#fff',
     marginBottom: 10,
+    fontSize: 16,
   },
   dateButton: {
-    padding: 10,
+    padding: 12,
     backgroundColor: '#007bff',
-    borderRadius: 5,
+    borderRadius: 8,
     marginBottom: 10,
   },
   dateButtonText: {
     color: '#fff',
     textAlign: 'center',
+    fontSize: 16,
+    fontWeight: '600',
   },
   taskContainer: {
     flexDirection: 'row',
@@ -273,21 +278,31 @@ const styles = StyleSheet.create({
     padding: 15,
     marginBottom: 10,
     backgroundColor: '#fff',
-    borderRadius: 5,
-    borderColor: '#ddd',
-    borderWidth: 1,
+    borderRadius: 12,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
+    marginHorizontal: 10, // Dodano marginesy po bokach
+  },
+
+  listContainer: {
+    marginTop: 15, // Dodano odstęp między przyciskiem a listą
   },
   completedTaskContainer: {
     backgroundColor: '#d4edda',
   },
   todayTaskContainer: {
-    backgroundColor: '#ffebcd',
+    backgroundColor: '#ffeeba',
   },
   pastDeadlineTaskContainer: {
     backgroundColor: '#f8d7da',
   },
   taskText: {
-    fontSize: 16,
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#333',
   },
   completedTaskText: {
     textDecorationLine: 'line-through',
@@ -296,39 +311,92 @@ const styles = StyleSheet.create({
   deadlineText: {
     fontSize: 14,
     color: '#888',
+    marginTop: 5,
   },
   taskButtons: {
     flexDirection: 'row',
     alignItems: 'center',
   },
   completeButton: {
-    color: 'green',
-    marginRight: 10,
+    color: '#28a745',
+    marginRight: 12,
+    fontSize: 16,
+    fontWeight: '600',
   },
   editButton: {
-    color: 'blue',
-    marginRight: 10,
+    color: '#007bff',
+    marginRight: 12,
+    fontSize: 16,
+    fontWeight: '600',
   },
   deleteButton: {
-    color: 'red',
+    color: '#dc3545',
+    fontSize: 16,
+    fontWeight: '600',
   },
   modalContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(0,0,0,0.5)',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Półprzezroczyste czarne tło
   },
   modalContent: {
     width: '90%',
     padding: 20,
-    backgroundColor: '#fff',
-    borderRadius: 10,
+    backgroundColor: '#fff', // Białe tło modala
+    borderRadius: 12,
     elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
   },
   modalTitle: {
-    fontSize: 20,
+    fontSize: 22,
     fontWeight: 'bold',
     marginBottom: 15,
     textAlign: 'center',
+    color: '#333',
+  },
+  modalButton: {
+    padding: 12,
+    backgroundColor: '#007bff',
+    borderRadius: 8,
+    marginTop: 10,
+  },
+  modalButtonText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  modalCancelButton: {
+    padding: 12,
+    backgroundColor: '#dc3545',
+    borderRadius: 8,
+    marginTop: 10,
+  },
+  modalCancelButtonText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  addButton: {
+    padding: 15,
+    backgroundColor: '#28a745', // Zielony kolor dla wyróżnienia
+    borderRadius: 10, // Zaokrąglone rogi
+    alignItems: 'center',
+    marginTop: 10,
+    elevation: 5, // Dodanie cienia dla efektu "przycisku"
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3.84,
+  },
+  addButtonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
